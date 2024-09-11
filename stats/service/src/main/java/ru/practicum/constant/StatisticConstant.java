8b13d4bd7b12150f8933e658a9fb8ef0ece05dd5
Ramoxa <ramoxa0007@yandex.ru>
2024-09-11 19:55:25 +0300

    v.1

--- stats/service/src/main/java/ru/practicum/constant/StatisticConstant.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.constant;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class StatisticConstant {
-    public static final String  DATE_TIME_FORMATTER = "yyyy-MM-dd HH:mm:ss";
-}
